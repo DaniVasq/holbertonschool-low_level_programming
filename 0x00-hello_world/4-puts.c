8a665e73cef08403a863361f80520cb01fcadb14
DaniVasq <danivasq50@gmail.com>
2019-02-07 19:30:21 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,5 +6,5 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ puts "Programming is like building a multilingual puzzle"); 
+{ int puts (const char "Programming is like building a multilingual puzzle"); 
 return (0); }
