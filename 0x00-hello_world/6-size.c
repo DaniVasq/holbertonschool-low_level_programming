80f202c5c1d021fff6a76d82f7f26dbc05dc04c7
DaniVasq <danivasq50@gmail.com>
2019-02-08 03:31:37 +0000

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%d\n",sizeof(char));
+	printf("%d\n",sizeof(int));
+	printf("%d\n",sizeof(long));
+	printf("%d\n",sizeof(long long));
+	printf("%d\n",sizeof(float));
+	return (0);
+}
