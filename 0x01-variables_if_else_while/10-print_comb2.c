5f939209f2922ad616ecec602cdff2192b618e3e
DaniVasq <danivasq50@gmail.com>
2019-02-09 03:05:49 +0000

    00..99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 48 ; a <= 57; a++)
+	{
+		for (b = 48 ; b <= 57; b++)
+		{
+		putchar(a);
+		putchar(b);
+	if (a + b < 114)
+	{
+		putchar(44);
+		putchar(32);
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
