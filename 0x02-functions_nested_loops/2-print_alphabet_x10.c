a1470895323bcec69665a4ee7df8fecbd7007104
DaniVasq <danivasq50@gmail.com>
2019-02-12 21:53:53 +0000

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * print_alphabet  - check the code for Holberton School students.
+ * print_alphabet_x10 - check the code for Holberton School students.
  *
- * Return: Always 0.
+ *
  */
 void print_alphabet_x10(void)
 {
