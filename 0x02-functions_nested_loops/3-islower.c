cc84ecb0b553bc00d71d4afa7d50dd89cce9e6f0
DaniVasq <danivasq50@gmail.com>
2019-02-12 22:57:57 +0000

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * _islower - checks lowercase c.
  *@c: It's the variable and you can check if c is lowercase or not.
  *
  *
