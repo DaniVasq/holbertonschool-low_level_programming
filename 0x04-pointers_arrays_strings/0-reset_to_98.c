2d9437c145ff44750e1a4ee2f92aa000a073c446
DaniVasq <danivasq50@gmail.com>
2019-02-19 23:35:52 +0000

    98 Battery St

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int and value to 98
+ * @n: parameter or number
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
