cf09f08bcdc66dc0d422839b305dcddbdc597985
DaniVasq <danivasq50@gmail.com>
2019-02-21 19:02:32 +0000

    strcat

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,20 @@
-hello
+#include "holberton.h"
+
+/**
+ * char *_strcat - concatenates two strings.
+ *
+ * Return: Always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	while (*dest)
+		dest++;
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return(0);
+}
