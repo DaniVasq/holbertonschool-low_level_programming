8af7ccbc3ddff5db4bd8d9a77ddc945da3773947
DaniVasq <danivasq50@gmail.com>
2019-02-22 00:11:29 +0000

    strncat

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,30 @@
-hello
+#include "holberton.h"
+
+/**
+ * char *_strncat - concatenates two strings until a certain point.
+ * @:dest - destination
+ * @:src - source
+ *
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+		int i = 0;
+		int i2 = 0;
+
+		while (dest[i])
+		{
+			i++;
+		}
+		while (src[i2])
+		{
+			dest[i + i2] = src[i2];
+			if (n < 3 &&
+			{
+				src[i2 +n] = '\0';
+			}
+			i++;
+		}
+		dest[i + i2] = src[i2];
+		return (dest);
+}
