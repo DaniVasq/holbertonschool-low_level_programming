b63c2ace5875a03fbcaeac848d86a7f3657adbbb
DaniVasq <danivasq50@gmail.com>
2019-02-26 21:20:51 +0000

    Chess

--- 0x06-pointers_arrays_strings/7-print_chessboard.c
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -2,15 +2,16 @@
 
 /**
  * print_chessboard - prints the chessboard.
+ * @a: - array
  */
 void print_chessboard(char (*a)[8])
 {
 	int w = 0;
 	int l = 0;
 
-	for (w = 0; a[w][l] != '\0'; w++)
+	for (w = 0; w < 8; w++)
 	{
-		for (l = 0; a[w][l] != '\0'; l++)
+		for (l = 0; l < 8; l++)
 		{
 			_putchar(a[w][l]);
 		}
