7efab6055b4f72871e69533fe281b9ba68d74915
DaniVasq <danivasq50@gmail.com>
2019-02-28 04:08:04 +0000

    Second try

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -3,16 +3,16 @@
 /**
  * _strlen_recursion - returns the length of a string.
  *@s: pointer/string
- *
+ *Return: 0 if statement is true, else return factorial
  */
 int _strlen_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
 	else
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
