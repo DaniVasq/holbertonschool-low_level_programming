297de7d4bc0dc3f4dddffb862a9bb1899b535ad2
DaniVasq <danivasq50@gmail.com>
2019-03-08 05:31:50 +0000

    Unfinished

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - cats two strings
+ *@n: number of bytes
+ *@s1: string
+ *@s2: second string
+ * Return: if function fails, NULL or pointer if successful
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int s1;
+	int s2;
+	int n;
+
+	return (NULL);
+}
