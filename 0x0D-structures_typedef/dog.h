5fbebc8771f0726d161feab66b41622f9185ca7e
DaniVasq <danivasq50@gmail.com>
2019-03-12 05:11:12 +0000

    Doggy dog

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -18,6 +18,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 typedef struct dog dog_t;
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+void print_dog(struct dog *d);
 
 #endif
