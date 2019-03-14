59db74ac334e75bd60494fb1536bc84d25971c02
DaniVasq <danivasq50@gmail.com>
2019-03-14 04:49:20 +0000

    Most hackers are young and hopefull

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * int main - function of opcode
+ *@argc: number of arguments
+ *@argv: array/vector
+ *Return: 0 Always (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	return(0);
+	}
