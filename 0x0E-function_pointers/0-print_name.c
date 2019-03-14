097f41ab5064198a507306fbd068b806adaeaa2e
DaniVasq <danivasq50@gmail.com>
2019-03-14 03:31:53 +0000

    Oh na na

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -2,8 +2,8 @@
 
 /**
  * print_name - prints a name as is
- *@name: person's/objects name
- *
+ *@name: person's  name
+ *@f: printer
  * Return: nothing (void)
  */
 void print_name(char *name, void (*f)(char *))
