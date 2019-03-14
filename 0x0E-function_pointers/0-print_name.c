67136bcd2ef5eda091a688b1201fe5fa258e4e0f
DaniVasq <danivasq50@gmail.com>
2019-03-14 03:24:34 +0000

    Oh na na

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name as is
+ *@name: person's/objects name
+ *
+ * Return: nothing (void)
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f(name);
+}
