39afe67511344fdafa345f0698b1573d9870ae8f
DaniVasq <danivasq50@gmail.com>
2019-03-14 04:27:04 +0000

    To hell

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ *@size: number of elements in array
+ *@cmp: pointer to the function to compare values
+ * Return: index of first element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (cmp && array)
+	{
+		if (size <= 0)
+			return(-1);
+
+		for (i = 0; i < size; i++)
+			{
+				if (cmp(array[i]) != 0)
+				   return (i);
+				   }
+			}
+			     return (-1);
+ }
