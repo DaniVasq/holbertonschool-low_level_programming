4d2a4ebefffce5aecf2ddd9087f07c41b0d0470f
DaniVasq <danivasq50@gmail.com>
2019-03-18 04:18:19 +0000

    Using only one while

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - function that prints anything.
+ *
+ *@format: list of types of arguments passed to the function
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *str;
+	unsigned int i = 0;
+
+	va_start(args, format);
+	while (format[i] && format != NULL)
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 's':
+			str = va_arg(args, char*);
+			if (!str)
+			{
+				printf("%p", str);
+				break;
+			}
+			printf("%s", str);
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		default:
+			i++;
+			continue;
+	}
+		if (format[i + 1] != 0)
+			printf(",");
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
