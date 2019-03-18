72ae09dec433ff70f0afebd6fa41895060bbafe6
DaniVasq <danivasq50@gmail.com>
2019-03-18 04:48:17 +0000

    changed from digit to int

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -16,7 +16,7 @@ void print_all(const char * const format, ...)
 
 	va_start(args, format);
 	i = 0;
-	while (format[i] && format != 0)
+	while (format && format[i] != 0)
 	{
 		switch (format[i])
 		{
