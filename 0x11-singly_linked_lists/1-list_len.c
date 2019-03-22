8c11363ab964ce5ab9368fffb15883828251a0c2
DaniVasq <danivasq50@gmail.com>
2019-03-22 01:49:45 +0000

    Edited for Betty

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 /**
  * list_len  - returns the number of elements in linked list.
- *
+ *@h: pointer in the node
  * Return: counter or i in this case.
  */
 size_t list_len(const list_t *h)
