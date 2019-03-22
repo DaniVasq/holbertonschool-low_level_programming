89ebfc64b93e1c831c4dcbc80c0fd7ea36ec1528
DaniVasq <danivasq50@gmail.com>
2019-03-22 01:47:14 +0000

    Added task 1 and header file.

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * list_len  - returns the number of elements in linked list.
+ *
+ * Return: counter or i in this case.
+ */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
