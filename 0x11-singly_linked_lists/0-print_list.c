af016bed92c0a2dc437e3b4135f19b55e843b076
DaniVasq <danivasq50@gmail.com>
2019-03-26 22:06:24 +0000

    Changed order of counter

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -11,16 +11,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
-		i++;
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
