b02adff878865430a97bf4c5113181edac383214
DaniVasq <danivasq50@gmail.com>
2019-03-27 02:44:56 +0000

    Third try

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -25,12 +25,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = newnode;
-		return (newnode);
 	}
-
+	else
+	{
 	if (endnode->next != NULL)
 		endnode = endnode->next;
 
 	endnode->next = newnode;
+	}
 	return (*head);
 }
