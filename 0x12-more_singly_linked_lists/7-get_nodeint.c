ec47214f7e26d19065e0e5817b85a30c652c545d
DaniVasq <danivasq50@gmail.com>
2019-03-27 04:17:01 +0000

    Fixed logic

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	point = head;
 
-	while (point != NULL && i == index)
+	while (point != NULL || i == index)
 	{
 		return (point);
 		point = point->next;
