e849c99dd7f33141d57eafbc830d6aab74d9d9fe
DaniVasq <danivasq50@gmail.com>
2019-03-27 04:13:38 +0000

    First commit

---
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * *get_nodeint_at_index - returns nth node of linked list
+ *@head: pointer pointing to head of list
+ *@index: index of the node
+ * Return: Always 0.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *point;
+	unsigned int i = 0;
+
+	point = head;
+
+	while (point != NULL && i == index)
+	{
+		return (point);
+		point = point->next;
+		i++;
+	}
+	return (NULL);
+}
