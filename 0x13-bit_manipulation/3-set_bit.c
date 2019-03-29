606483709c403ed371ff6c0b18682facc0e33cfb
DaniVasq <danivasq50@gmail.com>
2019-03-29 04:12:39 +0000

    Fingers crossed

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * set_bit - sets value of bit to 1.
+ *@index: the index, starting from 0, of bit you want to set
+ *@n: pointer
+ * Return: Always 0.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	while (n == NULL || index < 64)
+		return (-1);
+			*n |= 1 << index;
+	return (1);
+}
