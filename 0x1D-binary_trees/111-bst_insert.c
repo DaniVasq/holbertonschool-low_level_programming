bf31fff66f04ad56d681e75d9d5bfe8fbd05bb94
DaniVasq <danivasq50@gmail.com>
2019-07-25 19:05:29 +0000

    Uploading task 111

--- 0x1D-binary_trees/111-bst_insert.c
+++ 0x1D-binary_trees/111-bst_insert.c
@@ -1,9 +1,39 @@
 #include "binary_trees.h"
-
 /**
- * array_to_bst - builds a Binary Search Tree from an array
- * @array: pointer to the first element of the array to be converted
- * @size: number of element in the array
- * Return: pointer to the root node of the created BST, or NULL on failure
- **/
-bst_t *array_to_bst(int *array, size_t size)
+ * main - Entry point
+ * @tree: jkhsd
+ * @vale: lkasjdklsajd
+ * Return: Always 0 (Success)
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *aux = NULL;
+
+	if (tree == NULL)
+		return (NULL);
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+	aux = *tree;
+	if (value < aux->n)
+	{
+		if (aux->left == NULL)
+		{
+			aux->left = binary_tree_node(aux, value);
+			return (aux->left);
+		}
+		return (bst_insert(&(aux->left), value));
+	}
+	if (value > aux->n)
+	{
+		if (aux->right == NULL)
+		{
+			aux->right = binary_tree_node(aux, value);
+			return (aux->right);
+		}
+		return (bst_insert(&(aux->right), value));
+	}
+	return (NULL);
+}
