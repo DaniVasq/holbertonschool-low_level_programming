f988c893db1d244fa654930418fa7f417e1708ea
DaniVasq <danivasq50@gmail.com>
2019-12-06 19:44:42 +0000

    Pushing task 2

---
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts node as right child
+ *@parent: pointer to the node to intert right-chid
+ *@value: value to store in new node
+ * Return: returns pointer to the newnore of NULL if fail
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	if (parent == NULL)
+		return (NULL);
+	newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->right = NULL;
+	newnode->left = NULL;
+	if (parent->right != NULL)
+	{
+		newnode->right = parent->right;
+		newnode->right->parent = newnode;
+	}
+	parent->left = newnode;
+	return (newnode);
+}
