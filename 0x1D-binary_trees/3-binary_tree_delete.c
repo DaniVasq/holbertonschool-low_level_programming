47b820ce1664dd79be563203377b6f4bc16c9615
DaniVasq <danivasq50@gmail.com>
2019-12-06 20:14:39 +0000

    Fixing code

--- 0x1D-binary_trees/3-binary_tree_delete.c
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -3,12 +3,11 @@
 /**
  * binary_tree_delete - deletes a binary tree
  *@tree: pointer to root node of tree to delete
- * Return - nothing, void function
  */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree)
+	if (tree == NULL)
 		return;
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
