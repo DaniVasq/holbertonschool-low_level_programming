87027213fc443fa3a2eff17986ca70ba1a828ab4
DaniVasq <danivasq50@gmail.com>
2019-12-20 22:35:04 +0000

    Advanced task one and header update

---
+++ 0x1D-binary_trees/103-binary_tree_rotate_left.c
@@ -0,0 +1,28 @@
+#include "binary_tree.h"
+
+/**
+ * binary_tree_rotate_left -  left-rotation on a binary tree
+ * @tree: pointer to binary_tree_t
+ * Return: pointer to the new root node of the tree once rotated.
+ */
+
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *new;
+	binary_tree_t *tmp;
+
+	if (tree == NULL)
+		return (NULL);
+	if (tree->right)
+	{
+		tmp = tree->right->left;
+		new = tree->right;
+		new->parent = tree->parent;
+		tree->parent = new;
+		tree->right = tmp;
+		if (tmp)
+			tmp->parent = tree;
+		return (new);
+	}
+	return (NULL);
+}
