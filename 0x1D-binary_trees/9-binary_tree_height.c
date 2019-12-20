ea1ca195215e8debcb6c72eb2929ed47da7b81dc
DaniVasq <danivasq50@gmail.com>
2019-12-20 14:46:32 +0000

    Changing variable names

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -11,8 +11,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 
-		int i = 0;
-		int j = 0;
+		int left = 0;
+		int right = 0;
 
 		if (tree->right)
 			right = 1 + binary_tree_height(tree->right);
